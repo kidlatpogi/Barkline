3df5bd7d059ff71df14614a8baee1c6f5581b93a
kidlatpogi <dzeref4000@gmail.com>
2025-08-20 20:32:13 +0800

    landing gawa

--- Barkline/src/App.jsx
+++ Barkline/src/App.jsx
@@ -1,9 +1,9 @@
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./JSX/login";
 import Signup from "./JSX/signup";
+
+import LandingPage from "./JSX/landingPage";
 
 import ForgotPassword from "./JSX/forgotPassword";
 import ResetPassword from "./JSX/resetPassword";
@@ -12,11 +12,14 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
         {/* Login and Signup */}
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        {/* Forgot Password and Reset Password*/}
+        {/* Forgot Password and Reset Password */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
